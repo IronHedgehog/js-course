bcf6e82a123c3fd14c9dec708f329316dda84f14
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-10 08:04:16 +0300

    Changing Casing chalange fix

--- 1.6 Changing Casing/js/main.js
+++ 1.6 Changing Casing/js/main.js
@@ -1,4 +1,4 @@
 const firstName = prompt("Введіть імʼя");
 const capitalizeFirstLetter = firstName.slice(0, 1).toUpperCase();
 const restOfName = firstName.slice(1, firstName.length).toLowerCase();
-alert("Hello " + capitalizeFirstLetter + restName);
+alert("Hello " + capitalizeFirstLetter + restOfName);
