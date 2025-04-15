c737f0d86dd298d51aef51b60c21552479530baf
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-15 16:42:27 +0300

    Dom Chalange

--- DOM/2.9 Dom Chalange/index.js
+++ DOM/2.9 Dom Chalange/index.js
@@ -5,3 +5,7 @@ ul.lastElementChild.textContent = "Something new from JS";
 const link = document.querySelector(".list a");
 
 link.style.color = "red";
+
+const button = document.querySelector("button");
+
+button.style.backgroundColor = "yellow";
