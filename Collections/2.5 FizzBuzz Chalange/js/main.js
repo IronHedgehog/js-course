60616d366a316987a7609916d21da1c92824228d
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-15 09:36:00 +0300

    fizzBuzz

--- Collections/2.5 FizzBuzz Chalange/js/main.js
+++ Collections/2.5 FizzBuzz Chalange/js/main.js
@@ -1,20 +1,20 @@
 const array = [];
-let counter = 0;
+let counter = 1;
 
 function fizBuzGame() {
-  counter++;
-
-  if (counter % 3 === 0 && counter % 5 === 0) {
-    array.push("FizzBuzz");
-  } else if (counter % 5 === 0) {
-    array.push("Buzz");
-  } else if (counter % 3 === 0) {
-    array.push("Fizz");
-  } else {
-    array.push(counter);
+  while (counter <= 100) {
+    if (counter % 3 === 0 && counter % 5 === 0) {
+      array.push("FizzBuzz");
+    } else if (counter % 5 === 0) {
+      array.push("Buzz");
+    } else if (counter % 3 === 0) {
+      array.push("Fizz");
+    } else {
+      array.push(counter);
+    }
+    counter++;
   }
-}
-while (counter < 100) {
-  fizBuzGame();
   console.log(array);
 }
+
+fizBuzGame();
